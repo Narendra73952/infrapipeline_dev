6037347bd3f42efe9aa14d7b3e6a9b8f7ad528ab
unknown <narendrasinghgangwar>
2025-12-29 00:33:42 +0530

    provider block added

--- Enviroment/dev/Provider.tf
+++ Enviroment/dev/Provider.tf
@@ -5,10 +5,21 @@ terraform {
       version = "4.57.0"
     }
   }
+
+  
+  backend "azurerm" {
+    resource_group_name  = "sabkarg"  # Can also be set via `ARM_ACCESS_KEY` environment variable.
+    storage_account_name = "sabkasa"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "sabkaca"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "dev.terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
+  
 }
 
 provider "azurerm" {
   features {}
   subscription_id = "987e5914-628e-4e9a-8c8f-d7fa87735002"
 }
+
+
 
