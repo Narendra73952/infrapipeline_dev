0283685cf2f47b8618a9e12b895a97ed554afb1a
unknown <narendrasinghgangwar>
2026-01-06 15:52:39 +0530

    new backend code added

--- Enviroment/dev/Provider.tf
+++ Enviroment/dev/Provider.tf
@@ -8,9 +8,9 @@ terraform {
 
   
   backend "azurerm" {
-    resource_group_name  = "sabkarg"  # Can also be set via `ARM_ACCESS_KEY` environment variable.
-    storage_account_name = "sabkasa"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "sabkaca"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    resource_group_name  = "ng_rg"  # Can also be set via `ARM_ACCESS_KEY` environment variable.
+    storage_account_name = "ngstorage02"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "ngcong02"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "dev.terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
   
@@ -18,7 +18,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "987e5914-628e-4e9a-8c8f-d7fa87735002"
+  subscription_id = "716491cb-e2f6-4c7c-846f-b7d135de9a1e"
 }
 
 
